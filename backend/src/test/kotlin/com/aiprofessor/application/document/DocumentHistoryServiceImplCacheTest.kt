3c8e25c8464a76ee9927831b946dd5d1743ad565
leegyurak <devgyurak@igyulags-MacBook-Pro.local>
2025-10-13 17:54:54 +0900

    FIX: lint

--- backend/src/test/kotlin/com/aiprofessor/application/document/DocumentHistoryServiceImplCacheTest.kt
+++ backend/src/test/kotlin/com/aiprofessor/application/document/DocumentHistoryServiceImplCacheTest.kt
@@ -9,7 +9,6 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.data.domain.PageImpl
--- backend/src/test/kotlin/com/aiprofessor/infrastructure/util/PdfUtilsTest.kt
+++ backend/src/test/kotlin/com/aiprofessor/infrastructure/util/PdfUtilsTest.kt
@@ -22,24 +22,26 @@ class PdfUtilsTest {
     fun `should convert base64 to PDF bytes`() {
         // given - Create a minimal valid PDF
         val minimalPdf =
-            """%PDF-1.4
-1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj
-2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj
-3 0 obj<</Type/Page/Parent 2 0 R/MediaBox[0 0 612 792]/Contents 4 0 R>>endobj
-4 0 obj<</Length 44>>stream
-BT /F1 12 Tf 100 700 Td (Test PDF) Tj ET
-endstream endobj
-xref
-0 5
-0000000000 65535 f
-0000000009 00000 n
-0000000056 00000 n
-0000000109 00000 n
-0000000193 00000 n
-trailer<</Size 5/Root 1 0 R>>
-startxref
-284
-%%EOF""".trimIndent()
+            """
+            |%PDF-1.4
+            |1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj
+            |2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj
+            |3 0 obj<</Type/Page/Parent 2 0 R/MediaBox[0 0 612 792]/Contents 4 0 R>>endobj
+            |4 0 obj<</Length 44>>stream
+            |BT /F1 12 Tf 100 700 Td (Test PDF) Tj ET
+            |endstream endobj
+            |xref
+            |0 5
+            |0000000000 65535 f
+            |0000000009 00000 n
+            |0000000056 00000 n
+            |0000000109 00000 n
+            |0000000193 00000 n
+            |trailer<</Size 5/Root 1 0 R>>
+            |startxref
+            |284
+            |%%EOF
+            """.trimMargin()
 
         val base64 = Base64.getEncoder().encodeToString(minimalPdf.toByteArray())
 
