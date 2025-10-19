b8f2d32d177e85d95ccc4c952ca86381ff2600eb
leegyurak <dev@devgyurak.com>
2025-10-19 18:42:17 +0900

    FIX: fe build error

--- frontend/src/components/UserProfileModal.tsx
+++ frontend/src/components/UserProfileModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getUserInfo, deleteAccount, type UserInfoResponse } from '../apiClient';
 
 interface UserProfileModalProps {
